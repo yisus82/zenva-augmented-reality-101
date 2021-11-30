5e46560c397278ab568a2a5a8c3253080b658e8e
yisus82 <djalma8@gmail.com>
2021-11-30 13:38:51 +0100

    Scripting the Placement Indicator

--- IntroToARFoundation/Assets/Scripts/PlacementIndicator.cs
+++ IntroToARFoundation/Assets/Scripts/PlacementIndicator.cs
@@ -1,6 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class PlacementIndicator : MonoBehaviour
 {
+  private ARRaycastManager raycastManager;
+  private GameObject placementIndicator;
 
+  void Start()
+  {
+    raycastManager = FindObjectOfType<ARRaycastManager>();
+    placementIndicator = transform.GetChild(0).gameObject;
+    placementIndicator.SetActive(false);
+  }
+
+  void Update()
+  {
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+    if (hits.Count > 0)
+    {
+      placementIndicator.transform.position = hits[0].pose.position;
+      placementIndicator.transform.rotation = hits[0].pose.rotation;
+      placementIndicator.SetActive(true);
+    }
+    else
+    {
+      placementIndicator.SetActive(false);
+    }
+  }
 }
